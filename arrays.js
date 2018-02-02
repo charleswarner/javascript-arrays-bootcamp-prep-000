c9fd166874069c0d2933256917566b930dacfced
Charlie Warner <charlie@omnium-events.com>
2018-02-02 01:32:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array,element) {
+/*function addElementToBeginningOfArray(array,element) {
  array.unshift(element);
 
  return array;
-}
+} */
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
