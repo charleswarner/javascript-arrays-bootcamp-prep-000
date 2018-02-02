19d46bf3fe26f2af0249511e32a78aaf17787813
Charlie Warner <charlie@omnium-events.com>
2018-02-02 01:32:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,9 @@ function addElementToBeginningOfArray(array,element) {
 
  return array;
 }
-
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
+}
 function destructivelyAddElementToBeginningOfArray(array,element) {
 
 var newArray = [element, ...array];
